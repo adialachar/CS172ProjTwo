b99fa08052ee8b4b5728985f84e0d8fac9eb9294
Aditya Acharya <aacha002@ucr.edu>
2019-05-04 20:19:12 -0700

    last push

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ credentials['CONSUMER_KEY'] = 'vJsGsCMmiZapsIoEpYVahW76L'
 credentials['CONSUMER_SECRET'] = 'SVHB6pHLVZPOoppBYywcsXlRkxDGBzdLufDwoingipDoEH66lX'
 credentials['ACCESS_TOKEN'] = '3051117252-VLcksCEUJgk6kSRPjvkPqbZbT1jQC62cJHowgKq'
 credentials['ACCESS_SECRET'] = '48VbkQrhuieXAVwQObdvVuIBGDT5rCYCKjuh7FhCdVbqC'
-
+#Content-Type: text/html; charset=utf-8
 
 
 FILE = open('file.json','a')
@@ -41,32 +41,43 @@ class listener(StreamListener):
 
     
     def on_status(self,status):
-        if status.user.location is not None:
-            
-            dict = {'screen_name': status.user.screen_name, 'tweet': status.text, 'Location': status.user.location}
+
+        try:
             link = re.search("(?P<url>https?://[^\s]+)", status.text)
             if link is not None:
                 link = link.group("url")
-            if (link is not None):
-                    print(link)
-                    text = parse(urllib.request.urlopen(smart_str(link)))
-                    title = text.find('.//title').text
-                    dict['title'] = title
+                str(link)
 
 
-            print(dict)
-            if self.num_tweets < 500:
+       
+            if (len(status.entities['urls']) > 0):
+                
+                #print(status.entities['urls'][0]['expanded_url'])
+                link = status.entities['urls'][0]['expanded_url']
+         
+            dict = {'screen_name': status.user.screen_name, 'tweet': status.text, 'Location': status.user.location, 'Link': link}
+        
+
+        
+
+
+            if self.num_tweets < 100:
 
                 FILE.write(json.dumps(dict))
                 FILE.write("\n")
 
 
+            
                 self.num_tweets += 1
-                print(self.num_tweets)
                 return True
             else:
                 return False
 
+
+
+
+        except urllib.error.HTTPError:
+            return True
 
     def on_error(self,status_code):
         if status_code == 420:
@@ -83,7 +94,93 @@ twitterStream = Stream(auth, a)
 TP = twitterStream.filter(languages = ["en"], track = ["the","a"])
 
 
+emptyTitle = False
+data = []
+with open('file.json', 'r+') as f:
 
+    for line in f:
+        
+        #print(json.loads(line))
+        
+        data.append(json.loads(line))
+        
+
+    
+
+
+
+open('file.json', 'w').close()
+
+for line in data:
+
+    link = line['Link']
+    #print(link)
+    if (link is not None):
+        if (len(link) > 23):
+            text = parse(urllib.request.urlopen(str(link)))
+            title = text.find(".//title").text
+            if title is not None:
+
+                line['title'] = title
+                FILE.write(json.dumps(line))
+                FILE.write("\n")
+
+
+
+
+
+
+
+
+
+
+
+
+#with open('file.json', 'w') as g:
+
+
+
+
+
+'''
+    for line in f:
+        data = json.loads(line)
+        data['title'] = "goodboy"
+        print(data)
+        f.seek(0)
+        f.write(json.dumps(data))
+        f.truncate()
+    
+        
+        print(json.loads(line))
+        data = json.loads(line)       
+        
+        link = data['Link']
+        print("HELLOOOOOOOO")
+        print(link)
+        print("HIIIIIIIIIII")
+        if (link is not None):
+            print(link)
+            if (len(link) < 23):
+                emptyTitle = True
+            text = parse(urllib.request.urlopen(str(link)))
+
+            title = text.find('.//title').text
+            print("JJJJJJJJJJ")
+            print(title)
+            print("KKKKKKKKKKK")
+            if emptyTitle == True:
+                title = ""
+                emptyTitle = False
+                
+                
+            if title is not None:
+                f.seek(0)
+                data['title'] = title
+                json.dump(data,f,indent = 4)
+                f.truncate()
+                                                                                                                    
+'''
 
 
 
